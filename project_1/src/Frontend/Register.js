00b9be280033fc90a0a4f1fcaf1cfc218f1597c3
EJGDLG <edwinjosegabriel@gmail.com>
2025-03-03 01:51:00 -0600

    log-reg

--- project_1/src/Frontend/Register.js
+++ project_1/src/Frontend/Register.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./css/log-Reg.css";
+import registerBg from "./css/login-pp.png";
 
 function Register() {
   const [email, setEmail] = useState("");
@@ -34,31 +36,49 @@ function Register() {
   };
 
   return (
-    <div>
+    <div className="login">
+      <img src={registerBg} alt="login background" className="login__img" />
+      
+      <form onSubmit={handleRegister} className="login__form">
       <h1>Registro</h1>
-      {message && <p>{message}</p>}
-      <form onSubmit={handleRegister}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+      {message && <p className="login__message">{message}</p>}
+
+      <div className="login__content">
+          <div className="login__box">
+            <i className="bx bx-user login__icon"></i>
+            <div className="login__box-input">
+              <input
+                type="email"
+                className="login__input"
+                placeholder=""
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <label className="login__label">Email</label>
+            </div>
+          </div>
+
+          <div className="login__box">
+            <i className="ri-lock-2-line login__icon"></i>
+            <div className="login__box-input">
+              <input
+                type="password"
+                className="login__input"
+                placeholder=""
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <label className="login__label">Contraseña</label>
+            </div>
+          </div>
         </div>
-        <div>
-          <label>Contraseña:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Registrarse</button>
+        <button type="submit" className="login__button">Registrarse</button>
+        <button  type="submit" onClick={() => navigate("../")} className="login__register">
+          Ir a Login
+        </button>
       </form>
-      <button onClick={() => navigate("/genre-selection")}>Ir a Login</button>
     </div>
   );
 }
